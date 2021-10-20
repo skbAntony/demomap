6d6b73540be5a49c74cefc6cc32c209c51805ca0
antony <skb47106guy@yahoo.com.tw>
2021-10-20 18:03:14 +0800

    '更新地圖'

--- js/map.js
+++ js/map.js
@@ -2,19 +2,40 @@ $(function() {
     var map = new Vue({
         el: '#vue',
         data: {
-            places: [],
+            // loading的畫面
             loading: true,
+            
+            // 地圖
             map: null,
+            
+            // 所有藥局的資料
             data: [],
+            search_data: [],
+            
+            // 選擇地址的地區
+            selected: null,
+            areas: [],
+            
+            // 座標
             lat: 0,
             lng: 0,
+            
+            // 地標
             usermarker: null,
+
+            // 輸入欄
+            search_input: '',
+
+            // 勾選目前還有賣大人小孩的口罩
+            checkboxes: [false, false],
         },
     
         methods: {
             geticon: function (type) {
                 return L.icon({
-                    iconUrl: `http://localhost/laraveldemo/demo/images/mark-${type}.png`,      
+                    iconUrl: `http://localhost/laraveldemo/demo/demomap/images/mark-${type}.png`,
+                    // github   
+                    // iconUrl: `https://skbantony.github.io/demomap/images/mark-${type}.png`,    
                     iconSize: [66, 90],
                     iconAnchor: [33, 90], // point of the icon which will correspond to marker's location
                     popupAnchor: [0, -80] // point from which the popup should open relative to the iconAnchor
@@ -63,7 +84,17 @@ $(function() {
                     // 監聽 marker 點擊事件
                     let that = this;
                     this.data[i].marker.on('dblclick', (e) => {
-                        console.log(e);
+                        // 取得經緯度
+                        this.lat = e.latlng.lat;
+                        this.lng = e.latlng.lng;
+                        // 把地圖中心移動到點擊位置
+                        this.map.setView(new L.LatLng(e.latlng.lat, e.latlng.lng));
+                        // 第二次點擊的位置後會先刪除已點擊過的位置
+                        if (this.usermarker !== null) {
+                            this.map.removeLayer(this.usermarker);
+                        }
+                        this.usermarker = L.marker([e.latlng.lat, e.latlng.lng], { icon: this.geticon('blue') });
+                        this.map.addLayer(this.usermarker);
                     });
                     // 放入 marker cluster
                     this.storeMarkers.addLayer(this.data[i].marker);
@@ -72,25 +103,25 @@ $(function() {
                 this.map.addLayer(this.storeMarkers);
             },
             getdata: function () {
+                // 撈出所有藥局的資料
                 let that = this;
                 $.ajax({
                     url: 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json',
                     type: 'get',
                     dataType: 'json',
                     success: function(data) {
-                        console.log(data.features);
                         that.data = data.features;
+                        that.search_data = data.features;
                         data.features.forEach(function(item, i) {
-                            console.log(item.properties.county)
                             if (item.properties.county !== '') {
-                                that.places.push(item.properties.county);
+                                that.areas.push(item.properties.county);
                             }
                         });
-                        var result = that.places.filter((item, index, arr) => {
+                        var result = that.areas.filter((item, index, arr) => {
                             return arr.indexOf(item) === index;
                         });
-                        result.sort();
-                        console.log(result);
+                        // result.sort();
+                        that.areas = result;
                         that.addmarkers();
                         that.loading = false;
                     },
@@ -99,6 +130,41 @@ $(function() {
                     }
                 });
             },
+            search: function() {
+                // 搜尋藥局
+                var input = this.search_input;
+                var test = this.search_data; 
+                let that = this;
+                that.data = [];
+                test.forEach(function(item, i) {
+                    // 根據關鍵字找出藥局
+                    if ((that.checkboxes[0] === false || (that.checkboxes[0] === true && item.properties.mask_adult > 0)) &&
+                        (that.checkboxes[1] === false || (that.checkboxes[1] === true && item.properties.mask_child > 0)) &&
+                        item.properties.name.includes(input)) {
+                        // 判斷下拉式地區
+                        if (that.selected === null || (that.selected !== null && that.selected === item.properties.county)) {
+                            that.data.push(item);
+                        }
+                    }
+                    
+                });
+            },
+            list_data: function (list_data) {
+                // console.log(list_data.properties.available);
+                var lng = list_data.geometry.coordinates[0];
+                var lat = list_data.geometry.coordinates[1];
+                // 取得經緯度
+                this.lat = lat;
+                this.lng = lng;
+                // 把地圖中心移動到點擊位置
+                this.map.setView(new L.LatLng(lat, lng));
+                // 第二次點擊的位置後會先刪除已點擊過的位置
+                if (this.usermarker !== null) {
+                    this.map.removeLayer(this.usermarker);
+                }
+                this.usermarker = L.marker([lat, lng], { icon: this.geticon('blue') });
+                this.map.addLayer(this.usermarker);
+            },
         },
     
         mounted: function() {
@@ -106,5 +172,10 @@ $(function() {
             this.getdata();
         },
     
+    });
+    $(".search_input").keydown(function (e) {
+        if (e.which == 13) {
+            map.search();
+        }
     });
 });
