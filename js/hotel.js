5fe06cb0e94ed51fd7edc45fdbfd69494bbd2a17
antony <skb47106guy@yahoo.com.tw>
2021-11-12 17:57:16 +0800

    '新增租借網站'

--- js/hotel.js
+++ js/hotel.js
@@ -1,31 +1,123 @@
 $(function() {
+    Vue.component('my-component', {
+        template: '<div><p>test</p></div>',
+    });
+    // 一般接收api資料
+    // let request = new XMLHttpRequest();
+    // console.log(request);
+    // request.open("get", "https://challenge.thef2e.com/api/thef2e2019/stage6/rooms", true);
+    // request.setRequestHeader("Accept", "application/json");
+    // request.setRequestHeader("Authorization", "Bearer i7FGYV0NJeyPIgd5KuTh1JEEzG1Hwk6xp3LLhUNxVX2sLqVH60D72VxJv2hs");
+    // request.send();
+    // request.onload = function() {
+    //     console.log(request.status);
+    //     if (request.status == 200) {
+    //         roomsdata = JSON.parse(request.response);
+    //         console.log(roomsdata);
+    //     }
+    // }
     var map = new Vue({
         el: '#vue',
         data: {
+            headers: {
+                authorization:'Bearer i7FGYV0NJeyPIgd5KuTh1JEEzG1Hwk6xp3LLhUNxVX2sLqVH60D72VxJv2hs',
+                accept:'application/json',
+            },
+            api_data: {},
+            scrollPause: false,
+            error: false,
+            loading: false,
+            url: null,
+            isShow: true,
+
+            room: {},
+            descriptionShort_text: [],
+            descriptionShort: [],
+            amenities_img: [],
+            imageUrl: {},
+        },
+        props: {},
+        watch: {
+            error: function() {
+                alert('Data source error');
+            }
+        },
+        computed: {},
+        template: '',
+        components: {
             
         },
-    
         methods: {
-            getRoom:function(data, id){
+            getRoom: function(data, id) {
                 if (id === undefined) {
                     return axios.get('https://challenge.thef2e.com/api/thef2e2019/stage6/rooms',data);
                 } else {
                     return axios.get('https://challenge.thef2e.com/api/thef2e2019/stage6/room/' + id,data);
                 }
             },
+            more_mouseover: function(){
+                this.scrollPause = true;
+            },
+            more_mouseout: function(){               
+                this.scrollPause = false;
+            },
+            more_click: function(id) {
+                this.isShow = false;
+                var that = this;
+                document.querySelector('.slideMenu').style.cssText = 'transform: translateX(calc(-100% + 50px)); background-color: #8c9a65;';
+                document.querySelector('.room').style.cssText = 'padding-left: 49px';
+                
+                this.getRoom({headers:this.headers}, id).then(function(resp){
+                    that.room = resp.data.room[0];
+                    that.descriptionShort.push(that.room.descriptionShort.GuestMin + ' ~ ' + that.room.descriptionShort.GuestMax);
+                    that.descriptionShort.push(that.room.descriptionShort.Bed[0] + ' bed');
+                    that.descriptionShort.push(that.room.name);
+                    that.descriptionShort.push(that.room.descriptionShort.Footage);
+                    that.descriptionShort.push(that.room.normalDayPrice);
+                    that.descriptionShort.push(that.room.holidayPrice);
+                    that.descriptionShort_text.push('Accommodated:');
+                    that.descriptionShort_text.push('Bathroom:');
+                    that.descriptionShort_text.push('Bed type:');
+                    that.descriptionShort_text.push('Room size:');
+                    that.descriptionShort_text.push('Weekday Rate:');
+                    that.descriptionShort_text.push('Weekend Rate:');
+                    that.amenities_img['Wi-Fi'] = 'wifi';
+                    that.amenities_img['Breakfast'] = 'coffee';
+                    that.amenities_img['Air-Conditioner'] = 'building';
+                    that.amenities_img['Child-Friendly'] = 'child';
+                    that.amenities_img['Great-View'] = 'envelope';
+                    that.amenities_img['Mini-Bar'] = 'glass';
+                    that.amenities_img['Pet-Friendly'] = 'smile-o';
+                    that.amenities_img['Refrigerator'] = 'asterisk';
+                    that.amenities_img['Room-Service'] = 'user';
+                    that.amenities_img['Smoke-Free'] = 'minus';
+                    that.amenities_img['Sofa'] = 'tree';
+                    that.amenities_img['Television'] = 'television';
+                }).catch(function(err){
+                    that.error = true;
+                });  
+            },
+            page_click: function() {
+                if (this.isShow === false) {
+                    this.amenities_img = [];
+                    this.descriptionShort = [];
+                    this.descriptionShort_text = [];
+                    this.isShow = true;
+                    document.querySelector('.slideMenu').style.cssText = 'transform: translateX(0%);background-color: #fff;';
+                    document.querySelector('.room').style.cssText = 'padding-left: var(--asideleft)';
+                }
+            },
         },
-    
+
         mounted: function() {
-            var headers = {
-                authorization:'Bearer i7FGYV0NJeyPIgd5KuTh1JEEzG1Hwk6xp3LLhUNxVX2sLqVH60D72VxJv2hs',
-                accept:'application/json'
-            };    
-            this.getRoom({headers:headers}, '3Elqe8kfMxdZv5xFLV4OUeN6jhmxIvQSTyj4eTgIowfIRvF4rerA2Nuegzc2Rgwu').then(function(resp){
-                console.log(resp.data);
+            this.url = window.location.pathname;
+            var that = this;
+            this.getRoom({headers:this.headers}).then(function(resp){
+                that.api_data = resp.data.items;
+                that.loading = true;
             }).catch(function(err){
-                console.log(err);
+                that.error = true;
             });  
-            this.$emit('update:open', true);  
         },
     
     });
